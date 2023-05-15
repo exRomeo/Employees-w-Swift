867fd9d2720cb087673df103195908f02bb1138e
Ramy Ashraf <Ramy3r@hotmail.com>
2023-05-15 03:17:56 +0300

    replaced sqlite3 with core data and implemented insert delete and select functions

--- url session/Database.swift
+++ url session/Database.swift
@@ -6,118 +6,76 @@
 //
 
 import Foundation
-import SQLite3
-class Database {
+import CoreData
+
+final class Database {
     
-    private let id = "id"
-    private let name = "employee_name"
-    private let salary = "employee_salary"
-    private let age = "employee_age"
+    private static let id = "id"
+    private static let name = "employee_name"
+    private static let salary = "employee_salary"
+    private static let age = "employee_age"
     
-    static var instance: Database = Database()
-    private var db: OpaquePointer? = nil
+    private init(){}
     
-    private init(){
-        let file = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    
+    class func insert(_ employee: [String: Any], into context: NSManagedObjectContext) {
+        let entity = NSEntityDescription.entity(forEntityName: "Employees", in: context)
         
-        let path = file?.appendingPathComponent("employees_db.sqlite").relativePath
-        
-        print("file created at = \(path!)")
-        
-        if sqlite3_open(path, &db) == SQLITE_OK {
-            print("opened db")
-            
-            let createTableString =
-                """
-                CREATE TABLE IF NOT EXISTS Employees (
-                \(id) INT PRIMARY KEY NOT NULL,
-                \(name) CHAR (255),
-                \(salary) INT NOT NULL,
-                \(age) INT NOT NULL
-                );
-                """
-            var createStatement: OpaquePointer?
-            if sqlite3_prepare(db, createTableString, -1, &createStatement, nil) == SQLITE_OK {
-                if sqlite3_step(createStatement) == SQLITE_DONE {
-                    print("Employees Table Created")
-                } else {
-                    print("Employees table not created")
-                }
-            } else {
-                print("Not Prepared!")
-            }
-        } else {
-            print("Couldn't open file")
+        let emp = NSManagedObject(entity: entity!, insertInto: context)
+        emp.setValue(employee[id] as! Int, forKey: id)
+        emp.setValue(employee[name] as! String, forKey: name)
+        emp.setValue(employee[salary] as! Int, forKey: salary)
+        emp.setValue(employee[age] as! Int, forKey: age)
+        print(employee[name] as! String)
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
         }
     }
     
     
-    func insert(_ employee: [String: Any]) {
-        let insertString = "INSERT INTO Employees(\(id), \(name), \(salary), \(age)) VALUES(?, ?, ?, ?);"
-        
-        var insertStatement: OpaquePointer?
-        if sqlite3_prepare_v2(db, insertString, -1, &insertStatement, nil) == SQLITE_OK {
-
-            let id: Int32 = Int32(employee[id] as! Int)
-            let name: NSString = employee[name] as! NSString
-            let salary: Int32 = Int32(employee[salary] as! Int)
-            let age: Int32 = Int32(employee[age] as! Int)
-            
-            sqlite3_bind_int(insertStatement, 1, id)
-            sqlite3_bind_text(insertStatement, 2, name.utf8String, -1, nil)
-            sqlite3_bind_int(insertStatement, 3, salary)
-            sqlite3_bind_int(insertStatement, 4, age)
-            if sqlite3_step(insertStatement) == SQLITE_DONE {
-                print("insertion success")
-
-            } else {
-                print("insertion failure")
-            }
-        } else {
-            print("insert Statement not prepared")
-        }
-    }
-    
-    
-    func select() -> Array<Dictionary<String, Any>> {
+    class func select(from context: NSManagedObjectContext) -> Array<Dictionary<String, Any>> {
         var employees = Array<Dictionary<String, Any>>()
-        let selectString = "SELECT * FROM Employees;"
-        var selectStatement: OpaquePointer?
-        if sqlite3_prepare_v2(db, selectString, -1, &selectStatement, nil) == SQLITE_OK {
-            while sqlite3_step(selectStatement) == SQLITE_ROW {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Employees")
+        do {
+            let emps = try context.fetch(fetchRequest)
+            for emp in emps {
                 var employee: Dictionary<String, Any> = Dictionary()
-                employee[id] = Int(sqlite3_column_int(selectStatement, 0))
-                employee[name] = String(cString: sqlite3_column_text(selectStatement, 1))
-                employee[salary] = Int(sqlite3_column_int(selectStatement, 2))
-                employee[age] = Int(sqlite3_column_int(selectStatement, 3))
+                employee[id] = emp.value(forKey: id) as! Int
+                employee[name] = emp.value(forKey: name) as! String
+                employee[salary] = emp.value(forKey: salary) as! Int
+                employee[age] = emp.value(forKey: age) as! Int
                 employees.append(employee)
+                
+                print(emp.value(forKey: name) as! String)
             }
-        } else {
-            print("select Statement not prepared")
-        }
-        return employees
-    }
-    
-    
-    
-    func remove(employee: [String: Any]) {
-        let employee_id = Int32(employee[id] as! Int)
-        let deleteString = "DELETE FROM Employees WHERE \(id) = \(employee_id)"
-        var deleteStatement: OpaquePointer?
-        if sqlite3_prepare_v2(db, deleteString, -1, &deleteStatement, nil) == SQLITE_OK {
-            if sqlite3_step(deleteStatement) == SQLITE_DONE {
-                print("Row Deleted")
-            } else {
-                print("Row Not Deleted")
-            }
-        } else {
-            print("Delete Statement Not Prepared")
+            return employees
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            return employees
         }
     }
     
-    func saveEmployees(employees: Array<[String: Any]>){
+    
+    
+    class func remove(_ employee: [String: Any], from context:NSManagedObjectContext) {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Employees")
+        fetchRequest.predicate = NSPredicate(format: "id == %id", (employee[id] as! Int))
+        do {
+            let employees = try context.fetch(fetchRequest) as! [NSManagedObject]
+            for emp in employees {
+                context.delete(emp)
+            }
+            try context.save()
+        } catch {
+            print("Error deleting Employee with id \(employee[id] as! Int)")
+        }
+    }
+    
+    class func saveEmployees(_ employees: Array<[String: Any]>, into context: NSManagedObjectContext){
         for employee in employees{
-            self.insert(employee)
+            self.insert(employee, into: context)
         }
     }
 }
--- url session/ViewController.swift
+++ url session/ViewController.swift
@@ -7,15 +7,22 @@
 
 import UIKit
 import Reachability
+import CoreData
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     @IBOutlet weak var tableView: UITableView!
     let networkIndicatior = UIActivityIndicatorView(style: .large)
     var dataArray: Array<Dictionary<String, Any>> = Array()
+    var context: NSManagedObjectContext?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        networkIndicatior.color = .red
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+        
+        networkIndicatior.color = .blue
         networkIndicatior.center = view.center
         networkIndicatior.startAnimating()
         view.addSubview(networkIndicatior)
@@ -57,7 +64,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        Database.instance.remove(employee: dataArray[indexPath.row])
+        
+        Database.remove(dataArray[indexPath.row], from: context!)
         dataArray.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .fade)
     }
@@ -75,9 +83,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             (data , response, error) in
             do {
                 //search for try! and try?
-                let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, Any>
+                let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! Dictionary<String, Any>
                 self.dataArray = json["data"] as! Array<Dictionary<String, Any>>
-                Database.instance.saveEmployees(employees: self.dataArray)
+                Database.saveEmployees(self.dataArray, into: self.context!)
                 DispatchQueue.main.async {
 
                     self.networkIndicatior.stopAnimating()
@@ -92,9 +100,80 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func getEmployeesFromDatabase(){
-        dataArray = Database.instance.select()
+        dataArray = Database.select(from: context!)
         networkIndicatior.stopAnimating()
         tableView.reloadData()
     }
+    
+    
+    
+    
+//    
+//    private let id = "id"
+//    private let name = "employee_name"
+//    private let salary = "employee_salary"
+//    private let age = "employee_age"
+//    
+//    
+//     func insert(_ employee: [String: Any]) {
+//        let entity = NSEntityDescription.entity(forEntityName: "Employees", in: context!)
+//        
+//        let emp = NSManagedObject(entity: entity!, insertInto: context)
+//        emp.setValue(employee[id] as! Int, forKey: id)
+//        emp.setValue(employee[name] as! String, forKey: name)
+//        emp.setValue(employee[salary] as! Int, forKey: salary)
+//        emp.setValue(employee[age] as! Int, forKey: age)
+//        print(employee[name] as! String)
+//        do {
+//            try context?.save()
+//        } catch let error as NSError {
+//            print(error.localizedDescription)
+//        }
+//    }
+//    
+//    
+//     func select() -> Array<Dictionary<String, Any>> {
+//        var employees = Array<Dictionary<String, Any>>()
+//        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Employees")
+//        do {
+//            let emps = try context?.fetch(fetchRequest)
+//            for emp in emps! {
+//                var employee: Dictionary<String, Any> = Dictionary()
+//                employee[id] = emp.value(forKey: id) as! Int
+//                employee[name] = emp.value(forKey: name) as! String
+//                employee[salary] = emp.value(forKey: salary) as! Int
+//                employee[age] = emp.value(forKey: age) as! Int
+//                employees.append(employee)
+//                
+//                print(emp.value(forKey: name) as! String)
+//            }
+//            return employees
+//        } catch let error as NSError {
+//            print(error.localizedDescription)
+//            return employees
+//        }
+//    }
+//    
+//    
+//    
+//    func remove(_ employee: [String: Any]) {
+//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Employees")
+//        fetchRequest.predicate = NSPredicate(format: "id == id", (employee[id] as! Int))
+//        do {
+//            let employees = try context?.fetch(fetchRequest) as! [NSManagedObject]
+//            for emp in employees {
+//                context?.delete(emp)
+//            }
+//            try context?.save()
+//        } catch {
+//            print("Error deleting Employee with id \(employee[id] as! Int)")
+//        }
+//    }
+//    
+//    func saveEmployees(_ employees: Array<[String: Any]>){
+//        for employee in employees{
+//            self.insert(employee)
+//        }
+//    }
 }
 
